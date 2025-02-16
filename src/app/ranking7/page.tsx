1f3fce90af277c1704d807c1d40fd116fdd53e5e
isisnaldojoao <isisnaldojoao@gmail.com>
2025-02-16 19:32:30 -0300

    the new version with country for teams

--- src/app/ranking7/page.tsx
+++ src/app/ranking7/page.tsx
@@ -21,58 +21,58 @@ export default function Home() {
             </div>
             <Legend/>
 
-            <div className="grid grid-cols-4 bg-purple-500 h-[100px] gap-4 m-5 flex items-center justify-center rounded-lg transition-all hover:scale-105 ">
+            <div className="grid grid-cols-4 bg-gray-500 h-[100px] gap-4 m-5 flex items-center justify-center rounded-lg transition-all hover:scale-105 ">
                 <h1>1º |</h1>
                 <Image src="https://static.significados.com.br/flags/us.svg" width={100} height={50} alt="logo"/>
                 <h3  className='text-xl font-bold '>NRG</h3>
                 <div className="bg-white p-2 rounded-lg text-black ml-2">3000</div>
             </div>
 
-            <div className="grid grid-cols-4 bg-yellow-500 h-[100px] gap-4 m-5 flex items-center justify-center rounded-lg transition-all hover:scale-105 ">
+            <div className="grid grid-cols-4 bg-gray-500 h-[100px] gap-4 m-5 flex items-center justify-center rounded-lg transition-all hover:scale-105 ">
                 <h1>2º |</h1>
-                <Image src="https://static.significados.com.br/flags/br.svg" width={100} height={50} alt="logo"/>
+                <Image src="https://static.significados.com.br/flags/ua.svg" width={100} height={50} alt="logo"/>
                 <h3  className='text-xl font-bold '>Natus Vincere</h3>
                 <div className="bg-white p-2 rounded-lg text-black ml-2">2036</div>
             </div>
 
-            <div className="grid grid-cols-4 bg-purple-700 h-[100px] gap-4 m-5 flex items-center justify-center rounded-lg transition-all hover:scale-105 ">
+            <div className="grid grid-cols-4 bg-yellow-600 h-[100px] gap-4 m-5 flex items-center justify-center rounded-lg transition-all hover:scale-105 ">
                 <h1>3º |</h1>
-                <img src="https://i.imgur.com/iAGv6WN.png" width={100} height={50} alt="logo"/>
+                <img src="https://static.significados.com.br/flags/br.svg" width={100} height={50} alt="logo"/>
                 <h3  className='text-xl font-bold '>LOUD</h3>
                 <div className="bg-white p-2 rounded-lg text-black ml-2">1928</div>
             </div>
 
             <div className="grid grid-cols-4 bg-gray-500 h-[100px] gap-4 m-5 flex items-center justify-center rounded-lg transition-all hover:scale-105 ">
                 <h1>4º |</h1>
-                <Image src="https://static.significados.com.br/flags/gb.svg" width={100} height={50} alt="logo"/>
+                <Image src="https://static.significados.com.br/flags/kr.svg" width={100} height={50} alt="logo"/>
                 <h3  className='text-xl font-bold m-2'>DRX</h3>
                 <div className="bg-white p-2 rounded-lg text-black ml-2">1692</div>
             </div>
 
             <div className="grid grid-cols-4 bg-gray-500 h-[100px] gap-4 m-5 flex items-center justify-center rounded-lg transition-all hover:scale-105 ">
                 <h1>5º |</h1>
-                <Image src="https://static.significados.com.br/flags/kr.svg" width={100} height={50} alt="logo"/>
+                <Image src="https://static.significados.com.br/flags/sg.svg" width={100} height={50} alt="logo"/>
                 <h3  className='text-xl font-bold m-2'>Paper Rex</h3>
                 <div className="bg-white p-2 rounded-lg text-black ml-2">1430</div>
             </div>
 
             <div className="grid grid-cols-4 bg-gray-500 h-[100px] gap-4 m-5 flex items-center justify-center rounded-lg transition-all hover:scale-105 ">
                 <h1>6º |</h1>
-                <Image src="https://static.significados.com.br/flags/ru.svg" width={100} height={50} alt="logo"/>
+                <Image src="https://static.significados.com.br/flags/th.svg" width={100} height={50} alt="logo"/>
                 <h3  className='text-xl font-bold m-2'>TALON</h3>
                 <div className="bg-white p-2 rounded-lg text-black ml-2">1131</div>
             </div>
 
             <div className="grid grid-cols-4 bg-gray-500 h-[100px] gap-4 m-5 flex items-center justify-center rounded-lg transition-all hover:scale-105 ">
                 <h1>7º |</h1>
-                <Image src="https://static.significados.com.br/flags/sg.svg" width={100} height={50} alt="logo"/>
+                <Image src="https://static.significados.com.br/flags/gb.svg" width={100} height={50} alt="logo"/>
                 <h3 className='text-xl font-bold m-2'>Fnatic</h3>
                 <div className="bg-white p-2 rounded-lg text-black ml-2">1068</div>
             </div>
 
             <div className="grid grid-cols-4 bg-gray-500 h-[100px] gap-4 m-5 flex items-center justify-center rounded-lg transition-all hover:scale-105 ">
                 <h1>8º |</h1>
-                <Image src="https://static.significados.com.br/flags/lu.svg" width={100} height={50} alt="logo"/>
+                <Image src="https://static.significados.com.br/flags/jp.svg" width={100} height={50} alt="logo"/>
                 <h3  className='text-xl font-bold m-2'>ZETA DIVISION</h3>
                 <div className="bg-white p-2 rounded-lg text-black ml-2">930</div>
             </div>
@@ -86,7 +86,7 @@ export default function Home() {
 
             <div className="grid grid-cols-4 bg-gray-500 h-[100px] gap-4 m-5 flex items-center justify-center rounded-lg transition-all hover:scale-105 ">
                 <h1>10º |</h1>
-                <Image src="https://static.significados.com.br/flags/us.svg" width={100} height={50} alt="logo"/>
+                <Image src="https://static.significados.com.br/flags/br.svg" width={100} height={50} alt="logo"/>
                 <h3  className='text-xl font-bold m-2'>FURIA Esports</h3>
                 <div className="bg-white p-2 rounded-lg text-black ml-2">568</div>
             </div>
