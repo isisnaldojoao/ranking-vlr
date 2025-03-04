a51dfa66e798469d2f32f16d82202424b6980907
isisnaldojoao <isisnaldojoao@gmail.com>
2025-03-04 14:27:57 -0300

    the new commit, this is version for all

--- src/app/ranking19/page.tsx
+++ src/app/ranking19/page.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+
 import Image from 'next/image';
 import Legend from '../../components/legend/legends';
 import Header from '../../components/header/header';
--- src/app/ranking20/page.tsx
+++ src/app/ranking20/page.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+
 import Image from 'next/image';
 import Legend from '../../components/legend/legends';
 import Header from '../../components/header/header';
@@ -39,7 +39,7 @@ export default function Home() {
 
             <div className="grid grid-cols-4 bg-purple-700 h-[100px] gap-4 m-5 flex items-center justify-center rounded-lg transition-all hover:scale-105 ">
                 <h1>3º |</h1>
-                <img src="https://static.significados.com.br/flags/kr.svg" width={100} height={50} alt="logo"/>
+                <Image src="https://static.significados.com.br/flags/kr.svg" width={100} height={50} alt="logo"/>
                 <h3  className='text-xl font-bold '>Gen.G Esports</h3>
                 <div className="bg-white p-2 rounded-lg text-black ml-2">2176</div>
             </div>
